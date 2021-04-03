e91f3d84cfcfc2f6fa41f1ee465530a028068d61
Antonio Morales <amoralesch@hotmail.com>
2021-04-02 23:38:08 -0400

    Make sure unit tests are been executed

--- web/src/test/java/com/amoralesch/vdp/web/controller/StatusControllerTest.java
+++ web/src/test/java/com/amoralesch/vdp/web/controller/StatusControllerTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class StatusControllerTest {
+class StatusControllerTest {
   private final StatusController controller = new StatusController();
 
   @Test
